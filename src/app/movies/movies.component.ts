3f2aa8ce684f1a664a1fb7695a30a6d61e7414a6
JamalGuyo <jamalguyo08@gmail.com>
2021-06-17 14:34:11 +0300

    create movie template and use the api to populate

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -4,12 +4,29 @@ import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movies',
-  template: ` <p>{{ movie | json }}</p> `,
-  styles: [],
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
   searchTerm: any = 'iron man';
-  movie = {};
+  movies = [
+    {
+      Title: 'Iron Man',
+      Year: '2008',
+      imdbID: 'tt0371746',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Iron Man 3',
+      Year: '2013',
+      imdbID: 'tt1300854',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMjE5MzcyNjk1M15BMl5BanBnXkFtZTcwMjQ4MjcxOQ@@._V1_SX300.jpg',
+    },
+  ];
   constructor(
     private route: ActivatedRoute,
     private movieService: MovieService
@@ -21,6 +38,7 @@ export class MoviesComponent implements OnInit {
     });
     this.movieService
       .getMovies(this.searchTerm)
-      .subscribe((movie) => (this.movie = movie));
+      .subscribe((movie) => (this.movies = movie['Search']));
+    console.log(this.movies);
   }
 }
